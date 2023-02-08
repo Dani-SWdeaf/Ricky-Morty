c86cf2a1dcd945d024066bd1e6eae2af2ba8b4b3
unknown <fresstyle11@gmail.com>
2023-02-07 21:59:15 -0600

    APIs - Ricky

---
+++ src/components/Character.js
@@ -0,0 +1,53 @@
+export default function Characters(props) {
+
+    const { characters, setCharacters } = props;
+
+    const restCharacters = () => {
+        setCharacters(null);
+        // console.log("Resteando");
+    }
+    // console.log(characters);
+
+    return (
+        <div className="characters">
+            <h1>Personajes</h1>
+            <span className="back-home" onClick={restCharacters}>Volver a la home</span>
+            <div className="container-characters">
+                {characters.map((character, index) => (
+                    <div className="character-container" key={index}>
+                        <div>
+                            <img src={character.image} alt={character.name}></img>
+                        </div>
+                        <div>
+                            <h3>{character.name}</h3>
+                            <h6>
+                                {character.status === "Alive" ? (
+                                    <>
+                                        <span className="alive"></span>
+                                        Alive
+                                    </>
+                                ) : (
+                                    <>
+                                        <span className="dead"></span>
+                                        Dead
+                                    </>
+                                )}
+                            </h6>
+                            <p>
+                                <span className="text-grey">Episodios: </span>
+                                <span>{character.episode.length}</span>
+                            </p>
+                            <p>
+                                <span className="text-grey">Episodios:</span>
+                                1<span>{character.species}</span>
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+            <span className="back-home" onClick={restCharacters}>
+                Volver a la home
+            </span>
+        </div>
+    )
+}
